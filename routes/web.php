45817631d88c51d4adebd9ea160f71c9987af985
Dave Mills <d.e.mills@stats4sd.org>
2021-07-05 10:40:59 +0100

    add route defaults:  - include options for platforms with seperate front-end and backpack admin, and platforms with only admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * No seperate Front-end
+ *
+ * Use these routes for platforms that only use the
+ * Laravel Backpack interface and do not require a seperate
+ * front-end.
+ *
+ * The routes here will automatically redirect any users to the
+ * Backpack admin root. Logins will still be handled with Laravel / Breeze.
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(config('backpack.base.route_prefix'));
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get(config('backpack.base.route_prefix') . '/login', function () {
+    return redirect('login');
+});
+
+/**
+ * For platforms that *do* require a seperate front-end and
+ * admin panel, use the routes below. This will not automatically redirect.
+ * The groups are to seperate out front-end pages that are public vs
+ * front-end pages that require authentication.
+ */
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
+// Route::group([
+//     'middleware' => ['web', 'auth'],
+// ], function () {
+//     Route::view('dashboard')->name('dashboard');
+// });
 
 require __DIR__.'/auth.php';
