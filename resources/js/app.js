10a83d5d97c6b40aad07220e7aef842410fe5aa4
David Mills <d.e.mills@stats4sd.org>
2022-08-24 12:16:16 +0100

    remove vue2 syntax

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import _ from 'lodash';
-import Vue from 'vue';
+import {createApp} from 'vue';
 
 require('bootstrap');
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 
 window.Popper = require('popper.js').default;
@@ -11,15 +10,8 @@ window.$ = window.jQuery = require('jquery');
 
 import ExampleComponent from "./components/ExampleComponent.vue"
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
 window._ = _;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-Vue.component(ExampleComponent);
-
-new Vue({
-    el: '#app',
-});
+const app = createApp(ExampleComponent)
