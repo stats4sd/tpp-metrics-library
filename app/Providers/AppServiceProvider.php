df698db6a167cf27f8d30d13f8c969b2d8bf129b
Dave Mills <d.e.mills@stats4sd.org>
2023-01-18 17:51:28 +0000

    weird navigation setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,20 @@
 namespace App\Providers;
 
 use App\Filament\Resources\DimensionResource;
+use App\Filament\Resources\FrameworkResource;
+use App\Filament\Resources\MethodResource;
 use App\Filament\Resources\MetricPropertyResource;
 use App\Filament\Resources\MetricResource;
+use App\Filament\Resources\MetricUserResource;
 use App\Filament\Resources\PropertyTypeResource;
+use App\Filament\Resources\ScaleResource;
+use App\Filament\Resources\ToolResource;
 use App\Filament\Resources\TopicResource;
-use App\Models\Metric;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationBuilder;
 use Filament\Navigation\NavigationGroup;
 use Illuminate\Support\ServiceProvider;
+use pxlrbt\FilamentEnvironmentIndicator\FilamentEnvironmentIndicator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,6 +37,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // probably a better place for this...
+        FilamentEnvironmentIndicator::configureUsing(function (FilamentEnvironmentIndicator $indicator) {
+            $indicator->visible = fn() => auth()->user()?->hasRole('admin');
+        }, isImportant: true);
+
+        Filament::navigation(function (NavigationBuilder $builder): NavigationBuilder {
+            return $builder
+                ->item(...MetricResource::getNavigationItems())
+                ->groups([
+                    NavigationGroup::make('Topics')
+                        ->items([
+                            ...TopicResource::getNavigationItems(),
+                            ...DimensionResource::getNavigationItems(),
+                        ]),
+                    NavigationGroup::make('Tools, Methods + Frameworks')
+                        ->items([
+                            ...FrameworkResource::getNavigationItems(),
+                            ...MethodResource::getNavigationItems(),
+                            ...ToolResource::getNavigationItems(),
+                        ]),
+                    ])
+                ->items([
+                    ...MetricPropertyResource::getNavigationItems(),
+                    ...MetricUserResource::getNavigationItems(),
+                    ...ScaleResource::getNavigationItems(),
+                ]);
+        });
     }
 }
