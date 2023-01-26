a253a0c3020058a84b39fd0cc13fa6e1f6f17834
Dave Mills <d.e.mills@stats4sd.org>
2023-01-26 12:25:46 +0000

    WIP - rewrite metrics table

--- database/migrations/2022_12_07_144746_create_metrics_table.php
+++ database/migrations/2022_12_07_144746_create_metrics_table.php
@@ -15,15 +15,39 @@ return new class extends Migration
     {
         Schema::create('metrics', function (Blueprint $table) {
             $table->id();
+
+            // 1.f - derived+related metrics
             $table->foreignId('parent_id')->nullable()->constrained('metrics')->nullOnDelete()->cascadeOnUpdate();
-            $table->string('title');
-            $table->text('description')->nullable();
-            $table->text('references')->nullable();
-            $table->string('unit_of_measurement');
-            $table->string('study_unit');
-            $table->text('notes')->nullable();
             $table->text('parent_child_notes')->nullable();
+
+            // 0 - metric information
+            $table->string('title');
+            $table->foreignId('developer_id');
+
+            // 1 - Metric Description
+            $table->text('definition')->nullable();
+            $table->text('concept')->nullable();
+            $table->text('scale_notes')->nullable();
+            $table->text('units_of_measure_notes')->change();
+
+            $table->text('notes')->nullable();
+
+            // 4 Data
+            $table->foreignId('data_availability_id')->nullable();
+            $table->text('data_availability_notes')->nullable();
+
+            // 5.a
+            $table->text('pros_cons')->nullable();
+
+            // 5.b
+            $table->text('copmutation_methods')->nullable();
+
+            // 5.c
+            $table->boolean('scalable')->nullable();
+            $table->text('scalable_notes')->nullable();
+
             $table->timestamps();
+
         });
     }
 
