9085cc19629615bd72c3907204ee46b0b1cf8778
Dave Mills <d.e.mills@stats4sd.org>
2023-01-27 15:20:51 +0000

    fix merge issue

--- database/factories/PropertyOptionFactory.php
+++ database/factories/PropertyOptionFactory.php
@@ -9,11 +9,7 @@ class PropertyOptionFactory extends Factory
     public function definition(): array
     {
         return [
-<<<<<<< HEAD
             'name' => $this->faker->word(),
-=======
-            'label' => $this->faker->word(),
->>>>>>> main
             'notes' => $this->faker->paragraph(),
         ];
     }
