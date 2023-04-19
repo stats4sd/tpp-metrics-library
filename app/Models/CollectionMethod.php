0093cfdfcbdd65773bc1f3ecbb7d76376761465d
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2023-04-19 12:42:33 +0100

    adds collection method relation manager for metrics

--- app/Models/CollectionMethod.php
+++ app/Models/CollectionMethod.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class CollectionMethod extends Model
 {
@@ -13,9 +13,10 @@ class CollectionMethod extends Model
 
     protected $guarded = [];
 
-    public function metric(): BelongsTo
+    public function metrics(): BelongsToMany
     {
-        return $this->belongsTo(Metric::class);
+        return $this->belongsToMany(Metric::class, 'metric_collection_method')
+            ->withPivot('relation_notes');
     }
 
     public function properties(): MorphToMany
