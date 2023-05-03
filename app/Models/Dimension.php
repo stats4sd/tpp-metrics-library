542b072d2f1eb7a2f476cb5249312565a52eabeb
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2023-05-03 16:11:48 +0100

    sets up de-duplication bulk action

--- app/Models/Dimension.php
+++ app/Models/Dimension.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dimension extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = [];
 
