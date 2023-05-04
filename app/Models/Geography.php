da939120ef7e8167ae3dc83f3951b6fa864b4292
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2023-05-04 13:08:45 +0100

    updates notes to relation_notes

--- app/Models/Geography.php
+++ app/Models/Geography.php
@@ -23,6 +23,6 @@ class Geography extends Model
     public function references(): MorphToMany
     {
         return $this->morphToMany(Reference::class, 'referencable')
-            ->withPivot('reference_type', 'notes', 'id');
+            ->withPivot('reference_type', 'relation_notes', 'id');
     }
 }
--- app/Models/Scale.php
+++ app/Models/Scale.php
@@ -50,6 +50,6 @@ class Scale extends Model
     public function references(): MorphToMany
     {
         return $this->morphToMany(Reference::class, 'referencable')
-            ->withPivot('reference_type', 'notes', 'id');
+            ->withPivot('reference_type', 'relation_notes', 'id');
     }
 }
