a34d06044932dc103711ff0b519a3deac179c81a
Dave Mills <d.e.mills@stats4sd.org>
2023-05-16 18:15:54 +0100

    fix pivot var merge+reduce

--- app/Filament/Table/Actions/DeduplicateRecordsAction.php
+++ app/Filament/Table/Actions/DeduplicateRecordsAction.php
@@ -55,82 +55,83 @@ class DeduplicateRecordsAction extends BulkAction
 
                 // pre-load relations to avoid too many db calls
                 $records->load($relations);
+                $class = get_class($records->first());
+                $remaining_record = $class::findOrFail($record_remain_id);
 
-                // records level
-                $all_related_entries = $records->mapWithKeys(function (Model $record) use ($relations) {
+                // work 1 relation at a time
+                foreach ($relations as $relation) {
 
-                    // relations level
-                    $item = collect($relations)->mapWithKeys(function (string $relation) use ($record) {
+                    // records
+                    $all_related_records = $records->mapWithKeys(function (Model $record) use ($relation) {
                         $related_entities = $record->$relation;
 
                         $pivot_vars = $record->$relation()->getPivotColumns();
 
+                        // we have 'id' as a pivot Column on relationships where a single 'related' model can be linked to an entry multiple times via different relationships. (E.g. "references" vs "computation guidance" on Metrics).
+                        // this cannot be duplicated to a new pivot table entry, so filter it out.
+                        $pivot_vars = collect($pivot_vars)->filter(fn($var) => $var !== 'id');
+
+
                         $values = $related_entities->mapWithKeys(function (Model $related_entity) use ($pivot_vars) {
-                            $pivot_values = collect($pivot_vars)->mapWithKeys(function($pivot_var) use ($related_entity) {
-                               return [$pivot_var => $related_entity->pivot->$pivot_var];
+                            $pivot_values = $pivot_vars->mapWithKeys(function ($pivot_var) use ($related_entity) {
+                                return [$pivot_var => $related_entity->pivot->$pivot_var];
                             });
 
-                            return [ $related_entity->id => $pivot_values ];
+                            return [$related_entity->id => $pivot_values];
                         });
 
-                        return [$relation => $values];
+                        // cast record ID as string so merging doesn't remove them later.
+                        return [(string)$record->id => $values];
 
-                    });
+                    })->reduce(function ($carry, $item) use ($relation) {
 
-                    return [ $record->id => $item ];
-                });
+                        if ($relation === 'scaleDecision') {
 
-                dump($all_related_entries);
-
-
-                $
-
-
-                dd('hi');
-                $related_array = [];
-
-
-                // pre-fill related_array with keys, so we don't need to check if $related_array[$relation] exists or not;
-                foreach ($relations as $relation) {
-                    $related_array[$relation] = [];
-                }
-
-
-                foreach ($records as $record) {
-
-                    foreach ($relations as $relation) {
-
-                        $related_items = $record->$relation;
-
-                        $pivot_vars = $record->$relation()->getPivotColumns();
-
-                        foreach ($related_items as $related_item) {
-
-                            // if no entries for this $related_item->id are included yet, initialise.
-                            if (!isset($related_array[$relation][$related_item->id])) {
-                                $related_array[$relation][$related_item->id] = [];
-                            }
-
-                            foreach ($pivot_vars as $pivot_var) {
-
-                                // if no entries for this pivot_var and thie $related_item->id are included yet, initialise
-                                if (!isset($related_array[$relation][$related_item->id][$pivot_var])) {
-                                    $related_array[$relation][$related_item->id][$pivot_var] = '';
-                                }
-
-                                $related_array[$relation][$related_item->id][$pivot_var] .= '. ' . $related_item->pivot->$pivot_var;
-
-                            }
                         }
-                    }
 
-                }
+                        return $item->mapWithKeys(function (\Illuminate\Support\Collection $pivot_values, int $model_id) use ($carry) {
 
-                $class = get_class($records->first());
-                $remaining_record = $class::findOrFail($record_remain_id);
+                            // if model is already linked via relationship
+                            if (isset($carry[$model_id])) {
 
-                foreach ($relations as $relation) {
-                    $remaining_record->$relation()->sync($related_array[$relation]);
+                                $pivot_values = $pivot_values->mergeRecursive($carry[$model_id])
+                                    ->mapWithKeys(function ($value, $key) {
+
+                                        // if value now has multiple entries from the merge, merge/reduce them to a single value;
+                                        if (is_array($value)) {
+                                            $value = collect($value)->reduce(function ($current, $new) {
+                                                // if entries are identical, merge
+                                                if ($current === $new) {
+                                                    return $current;
+                                                }
+
+                                                // if entries are non-identical strings, concatenate;
+                                                if (is_string($new)) {
+                                                    return collect([$current, $new])->filter(fn($i) => $i !== null)->join('. ');
+                                                }
+
+                                                // for numeric, abritrarily return highest value (???)
+                                                if ($current && $current > $new) {
+                                                    return $current;
+                                                }
+
+                                                return $new;
+                                            });
+                                        }
+
+                                        return [$key => $value];
+                                    });
+                            }
+
+                            return [$model_id => $pivot_values];
+                        })->union($carry);
+
+                    }, collect([]));
+
+
+                    $remaining_record->$relation()->sync($all_related_records);
+
+
                 }
 
                 $class::whereIn('id', $records_remove)->delete();
