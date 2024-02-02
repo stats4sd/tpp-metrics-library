af13624750b016ff16e1ab752a7811f0da9b6e32
Dave Mills <d.e.mills@stats4sd.org>
2024-02-02 15:11:54 +0000

    remove call to old filament theming

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,33 +2,10 @@
 
 namespace App\Providers;
 
-use Filament\Facades\Filament;
-use Illuminate\Support\ServiceProvider;
-use App\Filament\Resources\FlagResource;
-use App\Filament\Resources\ToolResource;
-use App\Filament\Resources\UserResource;
-use Filament\Navigation\NavigationGroup;
-use App\Filament\Resources\ScaleResource;
-use App\Filament\Resources\TopicResource;
-use App\Filament\Resources\ImportResource;
 use App\Filament\Resources\MethodResource;
-use App\Filament\Resources\MetricResource;
-use Filament\Navigation\NavigationBuilder;
-use App\Filament\Resources\FeedbackResource;
-use App\Filament\Resources\PropertyResource;
-use App\Filament\Resources\DimensionResource;
-use App\Filament\Resources\FrameworkResource;
-use App\Filament\Resources\GeographyResource;
-use App\Filament\Resources\ReferenceResource;
-use App\Filament\Resources\MetricUserResource;
-use App\Filament\Resources\PropertyTypeResource;
-use App\Filament\Resources\SubDimensionResource;
-use App\Filament\Resources\FarmingSystemResource;
 use App\Filament\Resources\MetricPropertyResource;
-use App\Filament\Resources\DiscussionPointResource;
-use App\Filament\Resources\CollectionMethodResource;
-use Phpsa\FilamentAuthentication\Resources\RoleResource;
-use Phpsa\FilamentAuthentication\Resources\PermissionResource;
+use App\Filament\Resources\PropertyTypeResource;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -49,9 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Filament::serving(function () {
-            Filament::registerViteTheme('resources/css/filament.css');
-        });
+
 
 //        FilamentEnvironmentIndicator::configureUsing(function (FilamentEnvironmentIndicator $indicator) {
 //            $indicator->visible = fn() => auth()->user()?->hasRole('admin');
