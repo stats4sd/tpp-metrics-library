20750262d598aed391a35cb8f7ef879d4d09f030
Dan Tang <dan@stats4sd.org>
2024-02-20 09:49:20 +0000

    read csv file content into Laravel collection for further processing

--- app/Console/Commands/ImportCsvToolEvaluations.php
+++ app/Console/Commands/ImportCsvToolEvaluations.php
@@ -2,18 +2,15 @@
 
 namespace App\Console\Commands;
 
-use Carbon\Carbon;
 use App\Models\Tool;
 use App\Models\Scale;
-use App\Models\Currency;
 use App\Models\Developer;
 use App\Models\Dimension;
 use App\Models\Framework;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\DB;
-use App\Jobs\GetOneDayExchangeRates;
+
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 
 class ImportCsvToolEvaluations extends Command
@@ -37,106 +34,43 @@ class ImportCsvToolEvaluations extends Command
      */
     public function handle()
     {
+        $filename = 'storage/csv/tool_evaluations_18Oct23.csv';
+
         $this->info('start');
 
-        $filename = 'storage/csv/tool_evaluations_18Oct23.csv';
-        $delimiter = ',';
-        $lineNumber = 0;
 
-        // delete previous imported records permanently
+        // for testing purpose only, delete previous imported records permanently
         Tool::where('created_at', '>', '2024-02-18')->forceDelete();
         Developer::where('created_at', '>', '2024-02-18')->forceDelete();
         Dimension::where('created_at', '>', '2024-02-18')->forceDelete();
         Framework::where('created_at', '>', '2024-02-18')->forceDelete();
         Scale::where('created_at', '>', '2024-02-18')->forceDelete();
 
-        // return;
 
-        // ========== //
+        // ===== Read CSV file into Laravel collection ===== //
 
-        // open file
-        if (($handle = fopen($filename, 'r')) !== false) {
+        // 0. Read CSV file content
+        $csvFileContent = File::get($filename);
 
-            // handle each row
-            while (($row = fgetcsv($handle, 10000, $delimiter)) !== false) {
-                $lineNumber = $lineNumber + 1;
+        // 1. Split by new line. Use the PHP_EOL constant for cross-platform compatibility.
+        $lines = explode(PHP_EOL, $csvFileContent);
 
-                // skip first row for header
-                if ($lineNumber == 1) {
-                    continue;
-                }
+        // 2. Extract the header and convert it into a Laravel collection.
+        $header = collect(str_getcsv(array_shift($lines)));
 
-                $this->comment('*** Line ' . $lineNumber);
+        // 3. Convert the rows into a Laravel collection.
+        $rows = collect($lines);
 
-                // tool_name
-                $value = $this->getValue($row[5]);
-                $this->comment($value);
-
-                if ($value != null) {
-                    // update or create tools record
-                    $tool = Tool::updateOrCreate(['name' => $value]);
-                }
+        // 4. Map through the rows and combine them with the header to produce the final collection.
+        $data = $rows->map(fn ($row) => $header->combine(str_getcsv($row)));
 
 
-                // rayyan_ref
-                $this->comment($row[7]);
+        // ===== Handle CSV content line by line ===== //
 
-                // Question: do we need to find related metrics via rayyan_ref, then add relationship between metrics and tools?
-                // But we have not imported CSV file for metrics evaluations yet...
-                $rayyanRefs = str_getcsv($row[7], ';');
-                foreach ($rayyanRefs as $rayyanRef) {
-                    $this->comment(trim($rayyanRef));
-                }
-
-
-                // developer
-                $value = $this->getValue($row[10]);
-                $this->comment($value);
-
-                if ($value != null) {
-                    // update or create tools record
-                    $developer = Developer::updateOrCreate(['name' => $value]);
-                }
-
-
-                // dimensions
-                $value = $this->getValue($row[23]);
-                $this->comment($value);
-
-                if ($value != null) {
-                    // update or create tools record
-                    $dimension = Dimension::updateOrCreate(['name' => $value]);
-                }
-
-
-                // named_framework
-                $value = $this->getValue($row[32]);
-                $this->comment($value);
-
-                if ($value != null) {
-                    // update or create tools record
-                    $framework = Framework::updateOrCreate(['name' => $value]);
-                }
-
-
-                // scale_measure
-                $value = $this->getValue($row[41]);
-                $this->comment($value);
-
-                if ($value != null) {
-                    // update or create tools record
-                    $scaleMeasures = str_getcsv($value, ';');
-                    foreach ($scaleMeasures as $scaleMeasure) {
-                        $this->comment(trim($scaleMeasure));
-
-                        $scale = Scale::updateOrCreate(['name' => trim($scaleMeasure)]);
-                    }
-                }
-            }
-
-            // close file
-            fclose($handle);
+        foreach ($data as $row) {
+            $tool = Tool::updateOrCreate(['name' => $row['tool_name']]);
         }
+
 
         $this->info('done!');
     }
