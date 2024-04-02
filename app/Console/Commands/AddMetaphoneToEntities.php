9fa7e76e08e6ce7293b92b565861f61d414007d9
Dan Tang <dan@stats4sd.org>
2024-04-02 15:17:33 +0100

    Finetune metaphone

--- app/Console/Commands/AddMetaphoneToEntities.php
+++ app/Console/Commands/AddMetaphoneToEntities.php
@@ -29,15 +29,15 @@ class AddMetaphoneToEntities extends Command
     {
         $this->info('start');
 
-       // $this->addMetaphone(CollectionMethod::class, 'title');
+        // $this->addMetaphone(CollectionMethod::class, 'title');
         $this->addMetaphone(Dimension::class, 'name');
-//        $this->addMetaphone(Scale::class, 'name');
-//        $this->addMetaphone(Tool::class, 'name');
-//        $this->addMetaphone(Framework::class, 'name');
-//        $this->addMetaphone(Unit::class, 'name');
-//        $this->addMetaphone(MetricUser::class, 'name');
-//        $this->addMetaphone(Geography::class, 'name');
-//        $this->addMetaphone(Theme::class, 'name');
+        //        $this->addMetaphone(Scale::class, 'name');
+        //        $this->addMetaphone(Tool::class, 'name');
+        //        $this->addMetaphone(Framework::class, 'name');
+        //        $this->addMetaphone(Unit::class, 'name');
+        //        $this->addMetaphone(MetricUser::class, 'name');
+        //        $this->addMetaphone(Geography::class, 'name');
+        //        $this->addMetaphone(Theme::class, 'name');
 
         $this->info('done!');
     }
@@ -53,7 +53,15 @@ class AddMetaphoneToEntities extends Command
         $this->comment('Handling ' . count($entities) . ' ' . class_basename($entityModel) . ' entities...');
 
         foreach ($entities as $entity) {
-            $entity->metaphone = metaphone($entity->$columnName);
+            // by default, metaphone returns phonemes for full string.
+            // There will be very few possible duplicates detected.
+            //
+            // To finetune it, specify number of maximum phonemes to be returned.
+            // String with same spelling at forefront will be considered as possible duplicates.
+            //
+            // P.S. The number of maximum phonemes can be further finetuned.
+
+            $entity->metaphone = metaphone($entity->$columnName, 12);
             $entity->save();
         }
     }
